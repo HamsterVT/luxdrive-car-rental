c1fffdb07526874e3662308f705b33d44b0c91aa
HamsterVT <vladislav.aec@gmail.com>
2026-01-29 10:14:50 +0500

    Add missing settings import in fleet views

--- fleet_service/fleet/views.py
+++ fleet_service/fleet/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.utils import timezone
+from django.conf import settings
 from .models import Car, RentalRecord
 from .serializers import CarSerializer, CarListSerializer, AvailabilityCheckSerializer, RentalRecordSerializer
 import requests
